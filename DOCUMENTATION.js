a77bf24fc69613af9e5a1ca90315dbef2e4588a3
Metaphorme <77@diazepam.cc>
2023-10-07 13:36:36 +0800

    Initial commit

---
+++ DOCUMENTATION.js
@@ -0,0 +1,36 @@
+
+        function copy() {
+            var e = document.getElementById("copy");
+            e.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("Copied！");
+        }
+		function copy1() {
+            var e = document.getElementById("copy1");
+            e.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("Copied！");
+        }
+		function copy2() {
+            var e = document.getElementById("copy2");
+            e.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("Copied！");
+        }function copy3() {
+            var e = document.getElementById("copy3");
+            e.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("Copied！");
+        }
+		function copy4() {
+            var e = document.getElementById("copy4");
+            e.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("Copied！");
+        }
+		function copy5() {
+            var e = document.getElementById("copy5");
+            e.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("Copied！");
+        }
